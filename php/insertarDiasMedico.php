10805e0993566ca504437b4d767723fa63d901a1
MagarciaM <36037048+MagarciaM@users.noreply.github.com>
2018-02-05 12:02:54 +0100

    Add files via upload
    prueba 2

--- php/insertarDiasMedico.php
+++ php/insertarDiasMedico.php
@@ -2,19 +2,27 @@
  
 	include 'conexion.php';
 	global $conexion;
+
 	$json_dias = $_REQUEST['dates1'];
 	$id = $_REQUEST['id'];
-	echo($id);
-	echo($json_dias);
+
+	//echo($id);
+	//echo($json_dias);
+
 	$dias = json_decode($json_dias);
-	$dias.json_last_error();
-	echo $dias;
+	//$dias.json_last_error();
+
+	//print_r($dias);
+
 	// Separamos los dias y generamos un array con ellos
-	$array_dias = explode(",", $json_dias);
-	echo($array_dias);
+	/*$array_dias = explode(",", $json_dias);
+	print_r($array_dias);*/
+
 	$aux = "true";
-	for ($i=0 ; $i<count($array_dias) ; $i++) {
-		$ordenSQL = 'INSERT INTO dias_laborables (id_medico, fecha_laboral) VALUES("' . $id . '", "' . $array_dias[$i] . '")';
+
+	for ($i=0 ; $i<count($dias) ; $i++) {
+
+		$ordenSQL = 'INSERT INTO dias_laborales (id_medico, fecha_laboral) VALUES("' . $id . '", "' . $dias[$i] . '");';
 		
 		$res = $conexion->query($ordenSQL);
 		if (!$res) {
@@ -22,6 +30,8 @@
 			break;
 		}
 	}
+
+	//echo $ordenSQL;
 	echo $aux;
 
 ?>
